3d3e02bfa0a7b9a3af0ec319415018fb4ad8275b
darae <darae729@gmail.com>
2023-09-30 19:03:24 +0900

    add server

--- server.js
+++ server.js
@@ -6,9 +6,35 @@ let port = process.env.PORT || 7777;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message: 'hellow Express!'});
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+            'id' : 1,
+            'image' : 'https://picsum.photos/64/64',
+            'name' : '홍길동',
+            'birthday' : '950729',
+            'gender' : '남자',
+            'job' : '대학생'
+          },
+          {
+            'id' : 2,
+            'image' : 'https://picsum.photos/64/64',
+            'name' : '김개똥',
+            'birthday' : '930602',
+            'gender' : '남자',
+            'job' : '백수'
+          },
+          {
+            'id' : 3,
+            'image' : 'https://picsum.photos/64/64',
+            'name' : '황진이',
+            'birthday' : '960208',
+            'gender' : '여자',
+            'job' : '직장인'
+          }
+    ]);
 });
+
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
 
